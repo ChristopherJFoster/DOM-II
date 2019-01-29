4b8aa966e2e25a0eb3c22162f15276e87cadf1d8
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-29 15:55:29 -0500

    Added two more eventlistener types. Used preventDefault() on nav links.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 // listener type 1: mouseover
 // listerer type 2: mouseout
-// "Nest two similar events...prevent the event propagation"
+// "Nest two similar events somewhere in the site and prevent the event propagation properly"
 const navElement = document.querySelector("nav");
 navElement.addEventListener("mouseover", event => {
   navElement.style.backgroundColor = "darkgrey";
@@ -19,4 +19,16 @@ navContainer.addEventListener("mouseover", event => {
 });
 navContainer.addEventListener("mouseout", event => {
   navContainer.style.backgroundColor = "white";
+});
+
+// listener type 3: keydown
+const bodyElement = document.querySelector("body");
+bodyElement.addEventListener("keydown", event =>
+  alert("Why are you pressing a key?")
+);
+
+// listener type 4: click
+// "Stop the navigation items from refreshing the page by using preventDefault()"
+navElement.addEventListener("click", event => {
+  event.preventDefault();
 });
