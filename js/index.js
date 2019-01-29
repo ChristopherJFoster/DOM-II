2e1ee9be4dd19549cef6f33eee14bf331a9a88b3
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-29 15:27:35 -0500

    Added two eventlistener types. Nested two events and used stopPropagation().

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+// listener type 1: mouseover
+// listerer type 2: mouseout
+// "Nest two similar events...prevent the event propagation"
+const navElement = document.querySelector("nav");
+navElement.addEventListener("mouseover", event => {
+  navElement.style.backgroundColor = "darkgrey";
+  event.stopPropagation(); // The mouseover event on the nav element will not trigger the mouseover event on the .nav-container div, below.
+});
+navElement.addEventListener("mouseout", event => {
+  navElement.style.backgroundColor = "white";
+  event.stopPropagation();
+});
+
+const navContainer = document.querySelector(".nav-container");
+navContainer.addEventListener("mouseover", event => {
+  navContainer.style.backgroundColor = "DarkTurquoise";
+});
+navContainer.addEventListener("mouseout", event => {
+  navContainer.style.backgroundColor = "white";
+});
