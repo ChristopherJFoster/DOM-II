63b4aa242d893fd1b08f71ef66e40b19b97cd648
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-30 00:15:17 -0500

    Click event handler works. Moves block to top of stack.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,20 +1,33 @@
-const blockElements = document.querySelectorAll(".block");
-const blockElementsArray = Array.from(blockElements);
-const arrayContents = function(array) {
-  array.forEach(element => console.log(array.indexOf(element)));
+// I understood the instructions for Travelers to mean that when a block goes to the top of the stack, the other blocks do not change their order (they each stay in place or are shifted downward, depending on where the top-bound block was sitting). I think the code I wrote is a bit inelegant, but I'm happy that after some testing, I'm sure that when clicked, the blocks behave as I described above.
+
+// Elements grabbers
+const blockEls = document.querySelectorAll(".block");
+const blockElsArray = Array.from(blockEls);
+
+// Sets the flexbox order property of each array element equal to that element's index
+const orderToIndex = array => {
+  array.forEach(element => (element.style.order = array.indexOf(element)));
 };
 
-arrayContents(blockElementsArray);
+orderToIndex(blockElsArray);
 
-const clickListeners = blockElementsArray.forEach(element => {
+// I used this a lot to troubleshoot the click event handler below
+// const arrayContents = function(array) {
+//   array.forEach(element =>
+//     console.log(
+//       `${element.className} index:${array.indexOf(element)} order:${
+//         window.getComputedStyle(element).order
+//       }`
+//     )
+//   );
+// };
+
+// Upon clicking a block, moves that block to the top of the stack without otherwise changing the order of the blocks
+const clickListeners = blockElsArray.forEach(element => {
   element.addEventListener("click", event => {
-    elementIndex = blockElementsArray.indexOf(element);
-    blockElementsArray.splice(elementIndex, 1);
-    blockElementsArray.shift(element);
-    arrayContents(blockElementsArray);
-    // for (i = 0; i < elementIndex; i++) {
-    //   blockElementsArray[i].style.order += 1;
-    // }
-    // element.style.order = 0;
+    elementIndex = blockElsArray.indexOf(element);
+    blockElsArray.splice(elementIndex, 1);
+    blockElsArray.unshift(element);
+    orderToIndex(blockElsArray);
   });
 });
